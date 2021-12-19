867b0807b0faf95c9dd3e7b14b69184d4276154c
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-12-18 22:10:56 -0300

    fix(atualizando token): medida paliativa que funciona

--- src/provider/GenerateRefreshToken.ts
+++ src/provider/GenerateRefreshToken.ts
@@ -6,6 +6,23 @@ class GenerateRefreshToken {
     const prisma = new PrismaClient()
     const expiresIn = dayjs().add(15, 'second').unix()
 
+    const tokenExist = await prisma.refreshToken.findFirst({
+      where: {
+        userId
+      }
+    })
+
+    if (tokenExist) {
+      return await prisma.refreshToken.update({
+        where: {
+          userId
+        },
+        data: {
+          expiresIn
+        }
+      })
+    }
+
     await prisma.refreshToken.create({
       data: {
         userId,
