a28d6dd8536c107d457b7c324e7b6d7ecf844563
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-12-23 20:46:37 -0300

    fix(teste com segunda configuraçao): usando com rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import cors from 'cors'
 import CreateUserController from './controllers/CreateUserController'
 import LoginUserController from './controllers/LoginUserController'
 import ensureAuthenticated from './middlewares/ensureAutenticated'
@@ -10,19 +11,27 @@ const createUserController = new CreateUserController()
 const loginUserController = new LoginUserController()
 const authUserController = new AuthUserController()
 
+const corsOptions = {
+  origin: 'https://insta-alura-git-main-maxmillianox.vercel.app/',
+  optionsSuccessStatus: 200
+}
+
 router.post(
   '/cadastro',
+  cors(corsOptions),
   createUserController.handle
 )
 
 router.post(
   '/login',
+  cors(corsOptions),
   loginUserController.handle,
   ensureAuthenticated
 )
 
 router.post(
   '/auth',
+  cors(corsOptions),
   authUserController.handle
 )
 
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express from 'express'
 import router from './routes'
-import cors from 'cors'
 import 'express-async-errors'
 import handleError from './utils/HandleError'
 
 const app = express()
 
 app.use(express.json())
-
-app.use(cors({
-  origin: 'https://insta-alura-git-main-maxmillianox.vercel.app/',
-  optionsSuccessStatus: 200
-}))
 
 app.use(router)
 
